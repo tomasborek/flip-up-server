f8ebb8261bcb9990a9c7e5e3ed1754682e836d00
tomasborek <tomasborek3@gmail.com>
2023-06-07 15:05:14 +0200

    added new query params for categories

--- src/routes/category/controllers/getCategories.ts
+++ src/routes/category/controllers/getCategories.ts
@@ -6,49 +6,63 @@ type Data = z.infer<typeof getCategoriesQuerySchema>;
 
 export const getCategories = async (req: Request, res: Response) => {
   const query = req.query as Data;
-  try {
-    const categories = await prisma.category.findMany({
-      where: {
-        core: true,
+
+  let include = {};
+
+  if (Number(query.levels) === 1) {
+    include = {
+      subCategories: true,
+    };
+  } else if (Number(query.levels) === 2) {
+    include = {
+      subCategories: {
+        include: {
+          subCategories: true,
+        },
       },
-      ...(Number(query.levels) === 1
-        ? {
+    };
+  } else if (Number(query.levels) === 3) {
+    include = {
+      subCategories: {
+        include: {
+          subCategories: {
             include: {
               subCategories: true,
             },
-          }
-        : Number(query.levels) === 2
-        ? {
-            include: {
-              subCategories: {
-                include: {
-                  subCategories: true,
-                },
-              },
-            },
-          }
-        : Number(query.levels) === 3
-        ? {
-            include: {
-              subCategories: {
-                include: {
-                  subCategories: {
-                    include: {
-                      subCategories: true,
-                    },
-                  },
-                },
-              },
-            },
-          }
-        : {}),
+          },
+        },
+      },
+    };
+  }
+
+  if (query.parentCategoryIds) {
+    include = {
+      ...include,
+      parentCategories: {
+        select: {
+          id: true,
+        },
+      },
+    };
+  }
+
+  try {
+    const categories = await prisma.category.findMany({
+      where: {
+        core: query.core ? true : undefined,
+      },
+      ...(include ? { include } : {}),
     });
     return res.status(200).json({ categories });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const getCategoriesQuerySchema = z.object({
   levels: z.string().optional(),
+  core: z.enum(["true"]).optional(),
+  parentCategoryIds: z.enum(["true"]).optional(),
+  exportFormat: z.enum(["true"]).optional(),
 });
