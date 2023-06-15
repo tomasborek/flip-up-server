45a138b2f71c248629591bb1e61aaf29df070f2a
Tomáš Bořek <tomasborek3@gmail.com>
2023-06-15 11:27:56 +0200

    delete listing route

---
+++ src/routes/listing/controllers/deleteListing.ts
@@ -0,0 +1,30 @@
+import type { Request, Response } from "express";
+import { prisma } from "@db/prisma";
+import fs from "fs";
+
+export const deleteListing = async (req: Request, res: Response) => {
+  const { listingId } = req.params;
+
+  try {
+    const listing = await prisma.listing.findUnique({
+      where: { id: Number(listingId) },
+      include: { images: true },
+    });
+    if (!listing) return res.status(404).send({ message: "Listing not found" });
+    if (req.user!.id !== listing.userId)
+      return res.status(403).send({ message: "Forbidden" });
+    if (listing.images) {
+      listing.images.forEach((image) => {
+        fs.unlinkSync(image.url.split("/").slice(-1)[0]);
+      });
+    }
+    await prisma.listing.delete({
+      where: { id: Number(listingId) },
+    });
+
+    return res.status(200).send(null);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
--- src/routes/listing/index.ts
+++ src/routes/listing/index.ts
@@ -11,6 +11,7 @@ import { checkQuery } from "@middlewares/checkQuery";
 import { unprotectedRoute } from "@middlewares/unprotectedRoute";
 import { likeListing } from "./controllers/likeListing";
 import { unlikeListing } from "./controllers/unlikeListing";
+import { deleteListing } from "./controllers/deleteListing";
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
@@ -26,4 +27,5 @@ router.post("/", checkData(listingSchema), createListing);
 router.post("/:listingId/image", upload.array("images[]", 6), addImages);
 router.post("/:listingId/likedBy", likeListing);
 router.delete("/:listingId/likedBy", unlikeListing);
+router.delete("/:listingId", deleteListing);
 export default router;
