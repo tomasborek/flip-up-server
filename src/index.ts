2acee71b1a690b3f8d314430a04f7c207a3a41ff
Tomáš Bořek <tomasborek3@gmail.com>
2023-06-16 08:49:43 +0200

    added port for heroku

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,6 @@ app.use((req, res) => {
   });
 });
 
-app.listen(8080, "::", () => {
+app.listen(Number(process.env.PORT) || 8080, "::", () => {
   console.log(`⚡ Server running on port ${process.env.SERVER_PORT || 8080}`);
 });
